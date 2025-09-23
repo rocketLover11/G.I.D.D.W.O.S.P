3221a98da9fcb5001c87a47b3c09adb028200858
rocketLover11 <gidafish@gmail.com>
2025-09-23 19:24:16 +0000

    Commit stuff

--- SQLiteTest.java
+++ SQLiteTest.java
@@ -1,3 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLiteTest {
-  
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:sqlite:db/test.db";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcUrl);
+            System.out.println("Connection to SQLite established.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                    System.out.println("Connection Closed");
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
